cccda6c36f30dd35b7b3b9ac9eddd35ce1ffb687
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-20 13:19:58 -0500

    Completed intro Data provider

---
+++ scripts/countries/iceland/icelandIntroProvider.js
@@ -0,0 +1,11 @@
+const icelandIntro = [
+  {
+    image: "./images/iceland/icelandmainphoto.jpg",
+    imageAlt: "iceland landscape with water in foreground and mountains in the background",
+    name: "Iceland"
+  }
+]
+
+export const useIcelandIntro = () => {
+  return icelandIntro.slice()
+}
