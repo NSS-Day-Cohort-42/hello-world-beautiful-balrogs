325cb57e1fac6c43497a09bdad865e1362762ea8
Jacob Eckert <jweckert17@gmail.com>
2020-07-20 13:29:00 -0500

    created taiwan head of state data provider and populated with head of state data

---
+++ scripts/countries/taiwan/taiwanHeadOfStateProvider.js
@@ -0,0 +1,28 @@
+const taiwanHeadsOfState = [
+  {
+    name: "Cai Yingwen (蔡英文)",
+    image: "./images/taiwan/caiYingwen.jpeg",
+    imageAlt: "President Cai Yingwen",
+    description: "Cai Yingwen is a politician who has served as President since 2016. She is the first woman to be elected president in Taiwanese history. She is a member of the Democratic Progressive Party and was re-elected for a second term in January 2020."
+  },
+  {
+    name: "Ma Ying-jeou (馬英九)",
+    image: "./images/taiwan/maYingJeou.jpeg",
+    imageAlt: "Ma Ying-jeou",
+    description: "Ma Ying-jeou is a politician who served as President from 2008 to 2016. He was previously the Mayor of Taipei from 1998 to 2006. He is a member of the Kuomintang (KMT) political party."
+  },
+  {
+    name: "Chen Shui-bian (陳水扁)",
+    image: "./images/taiwan/chenShuiBian.jpeg",
+    imageAlt: "Chen Shui-bian",
+    description: "Chen Shui-bian is a retired politician who served as President from 2000 to 2008. He was the first president elected from the Democratic Progressive Party, ending the Kuomintang's 55 years of continuous rule in Taiwan."
+  },
+  {
+    name: "Chiang Kai-Shek (蔣介石)",
+    image: "./images/taiwan/chiangKaiShek.jpeg",
+    imageAlt: "Chiang Kai-shek",
+    description: "Chiang Kai-shek was a Chinese nationalist politician who served as the leader of the Republic of China from 1928 to 1975. He is a somewhat contentious historical figure, as he was responsible for both great social reform and economic prosperity in Taiwan, as well as some questionable authoritarian power grabs and purges."
+  },
+]
+
+export const useTaiwanHeadsOfState = () => taiwanHeadsOfState.slice()
