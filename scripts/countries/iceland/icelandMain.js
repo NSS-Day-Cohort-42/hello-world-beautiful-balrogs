5f4fdb04b479a9fa5e23e0d7d2457da601958368
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-20 14:04:09 -0500

    Added a mockup of IcelandMain.js

---
+++ scripts/countries/iceland/icelandMain.js
@@ -0,0 +1,11 @@
+// Mockup of icelandMain
+
+// import { introList } from "../../introComponent/introList.js"
+// import { cityList } from "../../cityComponent/cityList.js"
+// import { landmarkList } from "../../landmarkComponent/landmarkList.js"
+// import { headOfStateList } from "../../headOfStateComponent/headOfStateList"
+
+// introList('iceland')
+// cityList('iceland')
+// landmarkList('iceland')
+// headOfStateList('iceland')
