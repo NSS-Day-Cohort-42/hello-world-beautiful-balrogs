65753b83dfa4e04332da827c408e48852696435c
Austin Ring <aring97@gmail.com>
2020-07-20 13:51:34 -0500

    finishing fiji data providers

--- scripts/countries/fiji/introProvider.js
+++ scripts/countries/fiji/introProvider.js
@@ -1,8 +1,8 @@
-const intro={
+const intro=[{
     image:"images/logo.png",
     imageAlt:"tropical fish naer a beach",
     name:"Fiji"
-}
+}]
 export const useFijiIntro=()=>{
     return useFijiIntro
 }
