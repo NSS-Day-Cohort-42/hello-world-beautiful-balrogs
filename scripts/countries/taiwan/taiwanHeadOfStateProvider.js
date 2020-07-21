15dcf4b0df24d4dc22b805da00c7b2b6bb7ebfb1
Jacob Eckert <jweckert17@gmail.com>
2020-07-21 10:18:38 -0500

    removed all hardcoded html from taiwan.html, implemented most of taiwanMain.js, fixed some image links for taiwan politician data

--- scripts/countries/taiwan/taiwanHeadOfStateProvider.js
+++ scripts/countries/taiwan/taiwanHeadOfStateProvider.js
@@ -7,19 +7,19 @@ const taiwanHeadsOfState = [
   },
   {
     name: "Ma Ying-jeou (馬英九)",
-    image: "./images/taiwan/maYingJeou.jpeg",
+    image: "./images/taiwan/maYingJeou.jpg",
     imageAlt: "Ma Ying-jeou",
     description: "Ma Ying-jeou is a politician who served as President from 2008 to 2016. He was previously the Mayor of Taipei from 1998 to 2006. He is a member of the Kuomintang (KMT) political party."
   },
   {
     name: "Chen Shui-bian (陳水扁)",
-    image: "./images/taiwan/chenShuiBian.jpeg",
+    image: "./images/taiwan/chenShuiBian.jpg",
     imageAlt: "Chen Shui-bian",
     description: "Chen Shui-bian is a retired politician who served as President from 2000 to 2008. He was the first president elected from the Democratic Progressive Party, ending the Kuomintang's 55 years of continuous rule in Taiwan."
   },
   {
     name: "Chiang Kai-Shek (蔣介石)",
-    image: "./images/taiwan/chiangKaiShek.jpeg",
+    image: "./images/taiwan/chiangKaiShek.jpg",
     imageAlt: "Chiang Kai-shek",
     description: "Chiang Kai-shek was a Chinese nationalist politician who served as the leader of the Republic of China from 1928 to 1975. He is a somewhat contentious historical figure, as he was responsible for both great social reform and economic prosperity in Taiwan, as well as some questionable authoritarian power grabs and purges."
   }
--- scripts/countries/taiwan/taiwanMain.js
+++ scripts/countries/taiwan/taiwanMain.js
@@ -1,14 +1,11 @@
-/*
-This is what taiwanMain.js will look like when we have created all of the list components below.
-
+import { navBarLinkList } from "../../navBarLinkComponent/navBarLinkList.js"
 import { introList } from "../../introComponent/introList.js"
 import { cityList } from "../../cityComponent/cityList.js"
-import { landmarkList } from "../../landmarkComponent/landmarkList.js"
-import { headOfStateList } from "../../headOfStateComponent/headOfStateList"
+// import { landmarkList } from "../../landmarkComponent/landmarkList.js"
+import { headOfStateList } from "../../headOfStateComponent/headOfStateList.js"
 
+navBarLinkList()
 introList('taiwan')
 cityList('taiwan')
-landmarkList('taiwan')
+// landmarkList('taiwan')
 headOfStateList('taiwan')
-
-*/
