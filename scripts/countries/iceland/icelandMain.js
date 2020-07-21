c6ba02c71bb380b11e8e60ce6444cca903868b54
Jacob Eckert <jweckert17@gmail.com>
2020-07-21 11:16:44 -0500

    added call to landmarkList for iceland, taiwan, and romania

--- scripts/countries/iceland/icelandMain.js
+++ scripts/countries/iceland/icelandMain.js
@@ -3,11 +3,11 @@
 import { navBarLinkList } from "../../navBarLinkComponent/navBarLinkList.js";
 import { introList } from "../../introComponent/introList.js";
 import { cityList } from "../../cityComponent/cityList.js"
-// import { landmarkList } from "../../landmarkComponent/landmarkList.js"
+import { landmarkList } from "../../landmarkComponent/landmarkList.js"
 import { headOfStateList } from "../../headOfStateComponent/headOfStateList.js"
 
 navBarLinkList()
 introList('iceland')
 cityList('iceland')
-// landmarkList('iceland')
+landmarkList('iceland')
 headOfStateList('iceland')
--- scripts/countries/taiwan/taiwanMain.js
+++ scripts/countries/taiwan/taiwanMain.js
@@ -1,11 +1,11 @@
 import { navBarLinkList } from "../../navBarLinkComponent/navBarLinkList.js"
 import { introList } from "../../introComponent/introList.js"
 import { cityList } from "../../cityComponent/cityList.js"
-// import { landmarkList } from "../../landmarkComponent/landmarkList.js"
+import { landmarkList } from "../../landmarkComponent/landmarkList.js"
 import { headOfStateList } from "../../headOfStateComponent/headOfStateList.js"
 
 navBarLinkList()
 introList('taiwan')
 cityList('taiwan')
-// landmarkList('taiwan')
+landmarkList('taiwan')
 headOfStateList('taiwan')
