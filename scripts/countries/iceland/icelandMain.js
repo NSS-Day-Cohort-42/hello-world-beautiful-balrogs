9fd1b37551ae097c95710aef90108cb714f3cb0b
Michael Baumer <meeesterbaumer@gmail.com>
2020-07-21 10:51:24 -0500

    Removed nav HTML

--- scripts/countries/iceland/icelandMain.js
+++ scripts/countries/iceland/icelandMain.js
@@ -1,10 +1,12 @@
 // Mockup of icelandMain
 
+import { navBarLinkList } from "../../navBarLinkComponent/navBarLinkList.js";
 import { introList } from "../../introComponent/introList.js";
 import { cityList } from "../../cityComponent/cityList.js"
 // import { landmarkList } from "../../landmarkComponent/landmarkList.js"
 import { headOfStateList } from "../../headOfStateComponent/headOfStateList.js"
 
+navBarLinkList()
 introList('iceland')
 cityList('iceland')
 // landmarkList('iceland')
