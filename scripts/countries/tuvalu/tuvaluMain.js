180dfbc3114b142970d55609239d500ae8582f9e
Michael-Tyler <miketylerabc@gmail.com>
2020-07-21 11:17:08 -0500

    invoked all functions in main

--- scripts/countries/tuvalu/tuvaluMain.js
+++ scripts/countries/tuvalu/tuvaluMain.js
@@ -1,11 +1,11 @@
 import { cityList } from "../../cityComponent/cityList.js"
 import { headOfStateList } from "../../headOfStateComponent/headOfStateList.js"
-// import { landmarkList } from "../../landmarkComponent/landmarkList.js"
+import { landmarkList } from "../../landmarkComponent/landmarkList.js"
 import { introList } from "../../introComponent/introList.js"
 import { navBarLinkList } from "../../navBarLinkComponent/navBarLinkList.js"
 
-// landmarkList('tuvalu')
-cityList('tuvalu')
-headOfStateList('tuvalu')
-introList('tuvalu')
 navBarLinkList()
+introList('tuvalu')
+cityList('tuvalu')
+landmarkList('tuvalu')
+headOfStateList('tuvalu')
